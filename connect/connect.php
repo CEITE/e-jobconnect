cc67beb9a57df9fde7cc0d29676fdde425ce0221
avendano008 <87066382+avendano008@users.noreply.github.com>
2025-07-29 22:27:17 +0800

    update

---
+++ connect/connect.php
@@ -0,0 +1,64 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ejobconnect";
+
+
+// $servername = "localhost";
+// $username = "u729039049_bcphr";
+// $password = "s/wfGKb5@Q";
+// $dbname = "u729039049_bcphr";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+  die("Connection Error: " . $conn->connect_error);
+}
+
+
+      date_default_timezone_set('Asia/Manila');
+
+      $datestamp = date("Y-m-d");
+      $timestamp = date("H:i:s");
+
+      $date_time = date("Y-m-d H:i:s");
+      $timestamp2 = date("H:i a");
+      $day=date('l');
+      $day_num=date('d');
+      $month=date('M');
+      $year=date('Y');
+
+      $date_now = date("d/m/Y");
+
+      $today_str = strtotime(date("Y-m-d H:i:s"));
+
+
+      $end_year =date('Y', strtotime('+1 years'));
+
+      $end_date = "03/30/".$end_year;
+
+      $datetime1 = strtotime($date_now);
+      $datetime2 = strtotime($end_date);
+
+      $secs = $datetime2 - $datetime1;// == <seconds between the two times>
+      $remaining_days = ($secs / 86400)-175;
+
+     
+
+function age($currentage){
+
+  //date in mm/dd/yyyy format; or it can be in other formats as well
+  $birthDate = date_format(date_create($currentage),"m/d/Y");
+  //explode the date to get month, day and year
+  $birthDate = explode("/", $birthDate);
+  //get age from date or birthdate
+  $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    ? ((date("Y") - $birthDate[2]) - 1)
+    : (date("Y") - $birthDate[2]));
+  echo $age;
+}
+
+?>
---
+++ login-function.php
@@ -0,0 +1,76 @@
+<?php
+		include'connect/connect.php';
+		session_start();
+
+		
+		
+		if(isset($_POST['email'])){
+			extract($_POST);
+			$passwords=md5($password);
+
+			$sql = "SELECT * FROM admin WHERE email='$email' LIMIT 1";
+			$result = $conn->query($sql);
+
+		
+			if ($result->num_rows > 0) {
+				$type="admin";
+			}
+
+			$sql = "SELECT * FROM user WHERE email='$email' LIMIT 1";
+			$result = $conn->query($sql);
+			
+			if ($result->num_rows > 0) {
+				$type="user";
+			}
+
+			
+
+
+			$sql = "SELECT * FROM $type WHERE email='$email' LIMIT 1";
+			$result = $conn->query($sql);
+
+			
+
+			if ($result->num_rows > 0) {
+			  // output data of each row
+			  while($row = $result->fetch_assoc()) {
+			  	extract($row);
+			    if($attemp>=3){
+			    	 header("location: ./login.php?error=Please Contact developer to retrieve your account. 3 Attemp already used");
+			    }else{
+			    	if($passwords==$password){
+				    	$_SESSION['id']=$id;
+				    	//$_SESSION['type']=$type;
+				    		
+
+				    		$sql = "UPDATE $type SET attemp='0' WHERE email='$email'";
+							if ($conn->query($sql) === TRUE) {
+							  //echo "Record updated successfully";
+								header("location: ".$type."/" );
+							} else {
+							  echo "Error updating record: " . $conn->error;
+							}
+							
+				    }else{
+
+				    	$attemp = $attemp + 1; 
+
+				    	$sql = "UPDATE $type SET attemp='$attemp' WHERE email='$email'";
+						if ($conn->query($sql) === TRUE) {
+						  //echo "Record updated successfully";
+							$trial = 3 - $attemp;
+							header("location: ./login.php?error=Wrong password you have ".$trial." more attemp");
+						} else {
+						  echo "Error updating record: " . $conn->error;
+						}
+				    	
+				    	
+				    }
+			    }
+			  }
+			} else {
+			 	 header("location: ./login.php?error=No account found");
+			}
+		}
+
+?>
